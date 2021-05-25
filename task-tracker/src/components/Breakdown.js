181711c735544c4e5ef4bc90561377bfb676424b
Jason <jasn9776@gmail.com>
2021-05-25 14:46:46 +1000

    made breakdown hardcoded and can remove

--- task-tracker/src/components/Breakdown.js
+++ task-tracker/src/components/Breakdown.js
@@ -1,16 +1,69 @@
 import {Link} from 'react-router-dom'
 import Button from './Button'
 import { FaTimes } from 'react-icons/fa'
-
+import { useState, useEffect } from 'react';
+import Step from './Step'
 const Breakdown = ({id}) => {
+
+  const data = [
+    {
+    id: 1,
+    text: 'Cut apples',
+  },
+    {
+    id: 2,
+    text: 'Clean plates',
+  },
+  ]
+
+  const[breakdown, setBreakdown] = useState(data)
+
+
+  const deleteStep = (id) => {
+    console.log('deleting step', id)
+    setBreakdown(breakdown.filter((step) => step.id !== id))
+  }
+
+  
+
+
+    // useEffect(() => {
+    //     const getTasks = async () => {
+    //       const tasksFromServer = await fetchTasks()
+    //       setTasks(tasksFromServer)
+    //     }
+    
+    //     getTasks()
+    //   }, [])
+    
+      //Fetch Tasks
+    //   const fetchBreakdown = async () => {
+    //     console.log(id)
+    //     const res = await fetch(`http://localhost:5000/breakdowns/${id}`)
+    //     const breakdowns = await res.json()
+    
+    //     return breakdowns
+    // }
+
+    // useEffect(() => {
+    //     const getTasks = async () => {
+    //       const breakdownFromServer = await fetchBreakdown()
+    //       setBreakdown(breakdownFromServer)
+    //       console.log(breakdown.id)
+    //     }
+    
+    //     getTasks()
+    //   }, [])
+    
+
+
     console.log(id)
     return (
         <footer>
 
-                    {`${id} `}
-                {/* <Button color='green' text='Close' onClick={onAdd}/>  */}
-                <FaTimes/>                   
-
+            {breakdown.map((step) => (
+                <Step id={step.id} text={step.text} onDelete={deleteStep}></Step> 
+            ))}
         </footer>
     )
 }
